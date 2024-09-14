3bf6e9afe378f97decc58a1b266454980a3d45c1
maxin <maxin@coolpad.com>
2024-09-14 10:45:13 +0800

    Optimize

--- src/com/android/launcher3/model/WorkspaceItemSpaceFinder.java
+++ src/com/android/launcher3/model/WorkspaceItemSpaceFinder.java
@@ -41,7 +41,7 @@ public class WorkspaceItemSpaceFinder {
      * @return screenId and the coordinates for the item in an int array of size 3.
      */
     public int[] findSpaceForItem(LauncherAppState app, BgDataModel dataModel,
-            IntArray workspaceScreens, IntArray addedWorkspaceScreensFinal, int spanX, int spanY) {
+                                  IntArray workspaceScreens, IntArray addedWorkspaceScreensFinal, int spanX, int spanY) {
         LongSparseArray<ArrayList<ItemInfo>> screenItems = new LongSparseArray<>();
 
         // Use sBgItemsIdMap as all the items are already loaded.
@@ -85,7 +85,7 @@ public class WorkspaceItemSpaceFinder {
         if (!found) {
             // Still no position found. Add a new screen to the end.
             screenId = LauncherSettings.Settings.call(app.getContext().getContentResolver(),
-                    LauncherSettings.Settings.METHOD_NEW_SCREEN_ID)
+                            LauncherSettings.Settings.METHOD_NEW_SCREEN_ID)
                     .getInt(LauncherSettings.Settings.EXTRA_VALUE);
 
             // Save the screen id for binding in the workspace
